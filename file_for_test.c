4ae9a9f7bfe6fad2f9b77aea3f37d2836964cfe1
MrZloHex <zlo.alex.it@gmail.com>
2021-05-16 22:05:47 +0300

    opend

---
+++ file_for_test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hi i'm here only for tests\n");
+    return 0;
+}
--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+ 
+#define MAXCHAR 1000
 
 int main() {
-    printf("cat\n");
+    FILE *file;
+    char str[MAXCHAR];
+    char* filename = "file_for_test.c";
+ 
+    file = fopen(filename, "r");
+
+    if (file == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+
+    while (fgets(str, MAXCHAR, file) != NULL)
+        printf("%s", str);
+
+    fclose(file);
+    printf("\n%s\n", str);
+
     return 0;
 }
