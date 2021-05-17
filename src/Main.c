0c242d22e7f680a4265de63d39ea789b16b180a0
MrZloHex <zlo.alex.it@gmail.com>
2021-05-17 21:57:31 +0300

    refactor nice

--- src/Main.c
+++ src/Main.c
@@ -1,25 +1,49 @@
 #include "Main.h"
 
+void print_number(unsigned int counter){
+    printf("%s%s", ITALIC, DIM);
+    if (counter < 10) {
+        MV_RIGHT(5);
+        printf("%d", counter);
+        MV_RIGHT(2);
+    }
+    else if (counter < 100) {
+        MV_RIGHT(4);
+        printf("%d", counter);
+        MV_RIGHT(2);
+    }
+    else if (counter < 1000) {
+        MV_RIGHT(3);
+        printf("%d", counter);
+        MV_RIGHT(2);
+    }
+    else if (counter < 10000) {
+        MV_RIGHT(2);
+        printf("%d", counter);
+        MV_RIGHT(2);
+    }
+    printf("%s", STANDART);
+}
 
 void print_line(unsigned short width){
     for (unsigned short i = 0; i < width; i++)
         printf("-");
+    NEW_LINE();
 }
 
 int main(int argc, char **argv) {
     //printf("\n%s\n", argv[1]);
 
-    // counter of strings   
-    unsigned int counter = 0;
     // amount of chars in single string
     const unsigned short MAXCHAR = 1000;
-    // detectting width of terminal
+    // detecting width of terminal
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     const unsigned short WIDTH = w.ws_col;
 
+
     // opening file for reading
-    FILE *file;
+    FILE* file;
     char str[MAXCHAR];
     // filename is first argument of executing
     char* filename = argv[1];
@@ -30,38 +54,32 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // drawing lines
-    printf("\x1B[1C");  
+
+    // drawing line
     print_line(WIDTH);
     // printing file name
-    printf("        |  File: %s%s%s", BOLD, filename, STADART);
+    MV_RIGHT(8);
+    STD_OFFSET();
+    printf("File: %s%s%s", BOLD, filename, STANDART);
     printf("\n");
     // drawing lines
     print_line(WIDTH);
 
 
-
+    // counter of strings   
+    unsigned int counter = 1;
     while (fgets(str, MAXCHAR, file) != NULL) {
+        print_number(counter);
+        STD_OFFSET();
+        printf("%s", str);
         counter++;
-
-        if (counter < 10)
-            printf("     %d  |", counter);
-        else if (counter < 100)
-            printf("    %d  |", counter);
-        else if (counter < 1000)
-            printf("   %d  |", counter);
-        else if (counter < 10000)
-            printf("  %d  |", counter);
-        
-        printf("  %s", str);
     }
 
-    printf("\n");
+    NEW_LINE();
     print_line(WIDTH);
 
 
     fclose(file);
-    printf("\n");
 
     return 0;
 }
--- src/Main.h
+++ src/Main.h
@@ -5,15 +5,18 @@
 
 #define ESC             "\x1B"
 
-#define STADART             "\x1B[0m"
+#define STANDART            "\x1B[0m"
 #define BOLD                "\x1B[1m"
 #define DIM                 "\x1B[2m"
 #define ITALIC              "\x1B[3m"
 #define UNDERLINE           "\x1B[4m"
 #define BLINK               "\x1B[5m"
 
-#define FUN_MV_UP(i)        printf("\x1B[%dA", (i))
-#define FUN_MV_DOWN(i)      printf("\x1B[%dB", (i))
-#define FUN_MV_RIGHT(i)     printf("\x1B[%dC", (i))
-#define FUN_MV_LEFT(i)      printf("\x1B[%dD", (i))
+#define MV_UP(i)        printf("\x1B[%dA", (i))
+#define MV_DOWN(i)      printf("\x1B[%dB", (i))
+#define MV_RIGHT(i)     printf("\x1B[%dC", (i))
+#define MV_LEFT(i)      printf("\x1B[%dD", (i))
+
+#define NEW_LINE()      printf("\n");
+#define STD_OFFSET()    printf("|\x1B[2C")
 
